e039c7a55df923dbbb94509bd4057ef9978bc256
Mina <minas@iqmetrix.com>
2012-07-27 15:13:54 -0700

    updated styles, however I didn't update the actual project file since it was not compatible with VS 10.

---
+++ EntityModelPOC/Controls/ExtendedThumb.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace EntityModelPOC.Controls
+{
+    public class ExtendedThumb : Thumb
+    {
+
+
+        public Brush DisabledBackgroundColor
+        {
+            get { return (Brush)GetValue(DisabledBackgroundColorProperty); }
+            set { SetValue(DisabledBackgroundColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty DisabledBackgroundColorProperty =
+            DependencyProperty.Register("DisabledBackgroundColor", typeof(Brush), typeof(ExtendedThumb), new UIPropertyMetadata(null));
+
+        public Brush HoverBackgroundColor
+        {
+            get { return (Brush)GetValue(HoverBackgroundColorProperty); }
+            set { SetValue(HoverBackgroundColorProperty, value); }
+        }
+        public static readonly DependencyProperty HoverBackgroundColorProperty =
+            DependencyProperty.Register("HoverBackgroundColor", typeof(Brush), typeof(ExtendedThumb), new UIPropertyMetadata(null));
+
+
+        public Brush NormalBackgroundColor
+        {
+            get { return (Brush)GetValue(NormalBackgroundColorProperty); }
+            set { SetValue(NormalBackgroundColorProperty, value); }
+        }
+        public static readonly DependencyProperty NormalBackgroundColorProperty =
+            DependencyProperty.Register("NormalBackgroundColor", typeof(Brush), typeof(ExtendedThumb), new UIPropertyMetadata(null));
+    }
+}
--- EntityModelPOC/MainWindow.xaml.cs
+++ EntityModelPOC/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using EntityModelPOC.Model;
 
 namespace EntityModelPOC
@@ -23,8 +13,10 @@ namespace EntityModelPOC
 	public partial class MainWindow : Window
 	{
 		private EntityModelModel Model { get; set; }
+        private Direction _currentDirection =Direction.Left ;
+	    private Storyboard _storyBoard;
 
-		public MainWindow()
+	    public MainWindow()
 		{
 			InitializeComponent();
 			Model = TryFindResource("Model") as EntityModelModel;
@@ -42,12 +34,14 @@ namespace EntityModelPOC
 
 		private void EntityResource_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
 		{
+		    _currentDirection = Direction.Right;
 			// slide in the addresses.
 			slide(this.Resources, this.Addresses, Direction.Right);
 		}
 
 		private void EntityAddressDetails_MouseDoubleClick(object sender, MouseButtonEventArgs e)
 		{
+            _currentDirection = Direction.Left;
 			// slide back, but this would actually select this address.
 			slide(this.Addresses, this.Resources, Direction.Left);
 		}
@@ -56,8 +50,11 @@ namespace EntityModelPOC
 		private void slide(Border oldVisual, Border newVisual, Direction direction)
 		{
 			var width = this.ActualWidth;
-			var animOut = new ThicknessAnimation(new Thickness(0), new Thickness(direction == Direction.Right ? -width : width, 0, direction == Direction.Right ? width : 0, 0), new Duration(TimeSpan.FromMilliseconds(500)));
-			var animIn = new ThicknessAnimation(new Thickness(direction == Direction.Right ? width : -width, 0, direction == Direction.Right ? 0 : width, 0), new Thickness(0), new Duration(TimeSpan.FromMilliseconds(500)));
+			var animOut = new ThicknessAnimation(new Thickness(0), new Thickness(direction == Direction.Right ? -width : width, 0, direction == Direction.Right ? width : 0, 0), new Duration(TimeSpan.FromMilliseconds(300)));
+			var animIn = new ThicknessAnimation(new Thickness(direction == Direction.Right ? width : -width, 0, direction == Direction.Right ? 0 : width, 0), new Thickness(0), new Duration(TimeSpan.FromMilliseconds(400)));
+            
+            animIn.EasingFunction = new ExponentialEase { EasingMode = EasingMode.EaseOut, Exponent = 4 };
+            animIn.EasingFunction = new ExponentialEase { EasingMode = EasingMode.EaseOut, Exponent = 3 };
 
 			Storyboard.SetTarget(animOut, oldVisual);
 			Storyboard.SetTargetProperty(animOut, new PropertyPath(MarginProperty));
@@ -65,16 +62,43 @@ namespace EntityModelPOC
 			Storyboard.SetTarget(animIn, newVisual);
 			Storyboard.SetTargetProperty(animIn, new PropertyPath(MarginProperty));
 
-			var storyboard = new Storyboard();
-			storyboard.Children.Add(animOut);
-			storyboard.Children.Add(animIn);
-			storyboard.Freeze();
-			BeginStoryboard(storyboard);
+            _storyBoard = new Storyboard();
+            _storyBoard.Children.Add(animOut);
+            _storyBoard.Children.Add(animIn);
+            _storyBoard.Completed += StoryBoardCompleted;
+            _storyBoard.Begin();
 		}
 
-		private void Addresses_GoBack(object sender, RoutedEventArgs routedEventArgs)
+        void StoryBoardCompleted(object sender, EventArgs e)
+        {
+            _storyBoard.Stop();
+            UpdateMarginValues();
+        }
+
+        private void UpdateMarginValues()
+        {
+            if (_currentDirection == Direction.Right)
+            {
+                Resources.Margin = new Thickness(-ActualWidth, 0, ActualWidth, 0);
+                Addresses.Margin = new Thickness(0);
+            }
+            else
+            {
+                Addresses.Margin = new Thickness(ActualWidth, 0, ActualWidth, 0);
+                Resources.Margin = new Thickness(0);
+            }
+        }
+
+
+	    private void Addresses_GoBack(object sender, RoutedEventArgs routedEventArgs)
 		{
+            _currentDirection = Direction.Left;
 			slide(this.Addresses, this.Resources, Direction.Left);
 		}
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateMarginValues();
+        }
 	}
 }
